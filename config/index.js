f70392b26dbdbc562275baf6ffcf0bb55f6b4bfd
MACBOOK <macbook@dynamic-005-005-000-205.5.5.pool.telefonica.de>
2022-02-03 07:30:25 +0700

    updata deploy

--- config/index.js
+++ config/index.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
-const path = require("path");
+const dotenv = require('dotenv')
+const path = require('path')
+dotenv.config()
 
 module.exports = {
-    rootPath: path.resolve(__dirname, '..'),
-    serviceName: process.env.SERVICE_NAME,
-    jwtKey: process.env.SECRET,
-    urlDb: process.env.MONGO_URL
+  rootPath: path.resolve(__dirname, '..'),
+  serviceName: process.env.SERVICE_NAME,
+  jwtKey: process.env.SECRET,
+  urlDb: process.env.MONGO_URL
 }
